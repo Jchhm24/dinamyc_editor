d7752b54fbc92d5fa9e4cacabc0b052e8bd3d84f
Jchhm24 <jcchm2004@gmail.com>
2025-07-06 19:41:04 -0600

    se termino la primera plantilla responsiva de prueba para su renderizado e interacción

--- src/components/plantillas/1/plantilla_1_edit.svelte
+++ src/components/plantillas/1/plantilla_1_edit.svelte
@@ -1,41 +1,103 @@
 <script lang="ts">
   import '@styles/global.css';
+  import ContainerShow from '@components/container-show.svelte';
   import type { Img } from '@interfaces/img.interface';
   import type { Title } from '@interfaces/title.interface';
   import type { Content } from '@interfaces/content.interface';
+  import { handleTextarea } from 'src/helpers/handler-textarea';
+  import ContainerPlantilla from '@components/container-plantilla.svelte';
 
-  // Definir las props con sus tipos
-  let { img, title, content }: { img: Img; title: Title; content: Content } =
-    $props();
+  let {
+    img: imgProp,
+    title: titleProp,
+    content: contentProp,
+  } = $props<{
+    img: Img;
+    title: Title;
+    content: Content;
+  }>();
 
-    const click = () => {
-      img.show = !img.show;
-      alert('Las imagen ya no se sera visibe, trabajando en las opciones para se vuelva visible');
+  // Crea copias locales reactivas
+  let img = $state({ ...imgProp });
+  let title = $state({ ...titleProp });
+  let content = $state({ ...contentProp });
+
+  const handleImageChange = (event: Event) => {
+    const target = event.target as HTMLInputElement;
+    const file = target.files?.[0];
+
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        img.src = e.target?.result as string;
+      };
+      reader.readAsDataURL(file);
     }
-
+  };
 </script>
 
-<section
-  class="flex flex-row items-start justify-center p-4 gap-4 w-full border-2 border-gray-300 rounded-lg"
->
-  {#if img.show}
-    <div class="w-1/2 relative group box-border border-2 border-black/0 rounded-lg rounded-tr-none hover:border-blue-500 hover:rounded-tr-none
-      flex items-top
-    ">
-      <img src={img.src} alt={img.alt} class="w-full h-auto rounded-lg group-hover:rounded-tr-none object-fit-cover" />
-      <button class="absolute z-10 border-2 border-blue-500 bg-blue-500 top-0 right-0 translate-x-[22px] w-min group-hover:block" onclick={click}>
-        👁️
-      </button>
+<ContainerPlantilla --height="330px">
+  <ContainerShow
+    bind:show={img.show}
+    --height="100%"
+    --width="100%"
+    --min-width="260px"
+    --max-width="1120px"
+  >
+    <label for="img-input" class="box-border w-full">
+      <img
+        src={img.src}
+        alt={img.alt}
+        class="h-full w-screen cursor-pointer rounded-lg object-cover group-hover:rounded-tr-none"
+      />
+      <input
+        type="file"
+        name="img-input"
+        id="img-input"
+        accept="image/*"
+        onchange={handleImageChange}
+        class="hidden"
+      />
+    </label>
+  </ContainerShow>
+
+  {#if title.show || content.show}
+    <div class="flex h-full w-full flex-col items-start justify-start">
+      <ContainerShow bind:show={title.show}>
+        <input
+          type="text"
+          placeholder="Escribe aquí el título de la publicación..."
+          class="w-full text-center text-2xl font-bold h-min"
+          bind:value={title.text}
+        />
+      </ContainerShow>
+
+      <ContainerShow bind:show={content.show} --width="100%">
+        <textarea
+          placeholder="Escribe aquí el contenido de la publicación..."
+          bind:value={content.text}
+          class="w-full resize-none overflow-hidden"
+          rows="12"
+          oninput={(event) => handleTextarea().handleRows(event, content)}
+          onkeydown={(event) => handleTextarea().handleKeydown(event, 12)}
+        ></textarea>
+      </ContainerShow>
     </div>
   {/if}
+</ContainerPlantilla>
 
-  <div class="flex flex-col items-start justify-center w-1/2">
-    {#if title.show}
-      <h1 class="text-2xl font-bold mt-4">{title.text}</h1>
-    {/if}
+<style>
+  @media screen and (width < 720px) { 
+    img {
+      width: 100%;
+    }
 
-    {#if content.show}
-      <p class="mt-2 text-gray-700">{content.text}</p>
-    {/if}
-  </div>
-</section>
+    div{
+      height: min-content;
+    }
+
+    textarea{
+      text-align: center;
+    }
+  }
+</style>
