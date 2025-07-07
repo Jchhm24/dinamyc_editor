813bf57aca8aa896315e8472e6c7409ffae07c36
Jchhm24 <jcchm2004@gmail.com>
2025-07-06 19:45:13 -0600

    contedor de las plantillas que se usaran mas adelante

---
+++ src/components/container-plantilla.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import '@styles/global.css';
+  import type { Snippet } from 'svelte';
+
+  let { children } = $props<{ children: Snippet }>();
+
+</script>
+
+<section
+  class="flex w-screen items-center justify-center gap-4 rounded-lg border-2 border-gray-300"
+>
+    {@render children()}
+</section>
+
+<!-- 
+    Este contendor se encargara de encapsular a las plantillas que se vayan creando
+-->
+
+<style>
+  section {
+    height: var(--height, auto);
+    flex-direction: var(--flex-direction, row);
+  }
+
+  @media screen and (width < 720px) {
+    section {
+      flex-direction: column;
+      height: min-content;
+    }
+  }
+</style>
